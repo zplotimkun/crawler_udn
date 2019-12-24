c19c6123520e020094984083e88f537a6b722689
zplotimkun <timkun_@hotmail.com>
2019-12-24 17:49:13 +0800

    Add lehman military

--- crawler_lehman.py
+++ crawler_lehman.py
@@ -1,37 +1,37 @@
 import time
-import schedule
+import json
 import requests
+import schedule
 
 from bs4 import BeautifulSoup
-from lxml import etree
 from datetime import date
 
 import input_sql
 
-def crawler_upmedia(mydb, category):
+def crawler_lehman(mydb, category):
     page = 1
-    today= date.today()
+    today = date.today()
     while True:
         data_list = []
-        url = 'https://www.upmedia.mg/news_list.php?currentPage={}&Type=157'.format(page)
-        session = requests.session()
+        url = 'http://123.57.143.90/art/articleHomeShows.htm'
+        session = requests.Session()
         headers = {
-            'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/79.0.3945.79 Chrome/79.0.3945.79 Safari/537.36'
+            'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/79.0.3945.79 Chrome/79.0.3945.79 Safari/537.36'
         }
-        html = session.get(url, headers=headers)
-        soup = BeautifulSoup(html.text, 'lxml')
-        article_list = soup.find_all('dd')
+        post_data = {
+            'pageSize':'15',
+            'curPage':'{}'.format(page)
+        }
+        data = session.post(url, data=post_data, headers=headers)
+        data_json = json.loads(data.text)
+        article_list = data_json['rows']
         print('第{}頁'.format(page))
         print('※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※')
         article_num = 0
-        for articles in article_list:
-            article = articles.find_all('a')
-            for article_detail in article:
-                article_href = article_detail.get('href')
-                if 'news_info' in article_href:
-                    article_title = article_detail.text
-                    break
-            article_url = 'https://www.upmedia.mg/{}'.format(article_href)
+        for article in article_list:
+            article_href = article['id']
+            article_url = 'http://123.57.143.90/art/show.htm?id={}'.format(article_href)
+            article_title = article['name']
             sel_sql = input_sql.select_sql(mydb, article_url)
             if article_num == 0 and (sel_sql == False or article_title.strip() == ''):
                 print('已到重複文章，結束此程式')
@@ -45,11 +45,10 @@ def crawler_upmedia(mydb, category):
             print('-----------------------------------------------------------')
             article_html = session.get(article_url, headers=headers)
             article_soup = BeautifulSoup(article_html.text, 'lxml')
-
-            article_content_list = article_soup.find_all("div", {"class": "editor"})
+            article_content_list = article_soup.find_all("p")
             text_list = []
             for content_text in article_content_list:
-                if content_text.text.strip() == '':
+                if content_text.text.strip() == '' or '雷曼军事网' in content_text.text:
                     continue
                 text_list.append(content_text.text.strip())
 
@@ -66,13 +65,13 @@ def crawler_upmedia(mydb, category):
             data_list.append(data)
         input_sql.insert_sql(mydb, data_list)
         page += 1
-
+        time.sleep(5)
 
 
 
 def main():
     mydb = input_sql.conn_sql()
-    crawler_upmedia(mydb, 'military')
+    crawler_lehman(mydb, 'military')
 
 
 if __name__ == '__main__':
